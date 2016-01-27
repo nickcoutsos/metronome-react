7fcb156405bb82b9267f607c43332c6d84a47fe1
Nick Coutsos <nick@coutsos.com>
2016-01-27 00:52:42 -0500

    Add "button flash" effects

--- src/components.js
+++ src/components.js
@@ -25,9 +25,29 @@ export class Meter extends React.Component {
 
 		return (
 			<div className="meter" onClick={this.props.onClick}>
-				<div className={needleClasses.join(' ')} style={style}>
-				</div>
+				<div className={needleClasses.join(' ')} style={style} />
 			</div>
+		);
+	}
+}
+
+export class Button extends React.Component {
+	handleClick(e) {
+		if (this.props.onClick) this.props.onClick(e);
+		this.flash();
+	}
+
+	flash() {
+		let classes = this.refs.el.classList;
+		classes.add('flash');
+		setTimeout(() => classes.remove('flash'), 100);
+	}
+
+	render() {
+		return (
+			<span ref="el" onClick={e => this.handleClick(e)} className={`button ${this.props.className}`}>
+				{this.props.children}
+			</span>
 		);
 	}
 }
@@ -36,9 +56,9 @@ export class Controls extends React.Component {
 	render() {
 		return (
 			<p className="controls">
-				<span onClick={e => this.props.onBpmDown()} className="button bpm-decrement">-</span>
+				<Button onClick={e => this.props.onBpmDown()} className="bpm-decrement">-</Button>
 				<span className="bpm-display">{this.props.bpm}</span>
-				<span onClick={e => this.props.onBpmUp()} className="button bpm-increment">+</span>
+				<Button onClick={e => this.props.onBpmUp()} className="bpm-increment">+</Button>
 			</p>
 		);
 	}
@@ -74,25 +94,6 @@ export class Metronome extends React.Component {
 	beat() {
 		this.state.beats % 2 == 0 ? this.playTick() : this.playTock();
 		this.setState({ beats: ++this.state.beats });
-
-		// this.metronome.query('.needle')
-		// 	.css({'background': '#FF0000'})
-		// 	.css({'background': 'gray'},
-		// 		 {'tween': this.seconds_per_beat() * 500});
-		// this.metronome.query('.meter')
-		// 	.animate(function(f) {
-		// 		var saturation = Math.round((1 - f) * 45 + 14),
-		// 			hsl_inner = 'hsl(210, ' + saturation + '%, ' + ((1 - f) * 5 + 82) + '%)',
-		// 			hsl_outer = 'hsl(210, ' + saturation + '%, ' + ((1 - f) * 20 + 64) + '%)',
-		// 			gradient = (
-		// 				'(bottom right, circle cover, '
-		// 				+ hsl_inner + ', '
-		// 				+ hsl_outer + ' 70%)'
-		// 			);
-
-		// 		this.css({'background': '-webkit-radial-gradient' + gradient});
-
-		// 	}, this.seconds_per_beat() * 500, 'ease');
 	}
 
 	setBpm(bpm) {
@@ -107,16 +108,10 @@ export class Metronome extends React.Component {
 
 	decrement(amount) {
 		this.setBpm(Math.max(1, this.state.bpm - (amount || 1)));
-
-		// this.metronome.query('.bpm-decrement').classify('flash');
-		// setTimeout(() => this.metronome.query('.bpm-decrement').declassify('flash'), 100);
 	}
 
 	increment(amount) {
 		this.setBpm(Math.min(200, this.state.bpm + (amount || 1)));
-
-		// this.metronome.query('.bpm-increment').classify('flash');
-		// setTimeout(() => this.metronome.query('.bpm-increment').declassify('flash'), 100);
 	}
 
 	handleScroll(e) {
@@ -148,9 +143,9 @@ export class Metronome extends React.Component {
 	stop() {
 		if (!this.state.running) return;
 
-		this.refs.meter.stop();
-
 		clearInterval(this.state.interval);
+
+		this.refs.meter.stop();
 		this.setState({
 			interval: null,
 			running: false
@@ -158,7 +153,9 @@ export class Metronome extends React.Component {
 	}
 
 	toggle() {
-		this.state.running ? this.stop() : this.start();
+		this.state.running ?
+			this.stop() :
+			this.start();
 	}
 
 	render() {
