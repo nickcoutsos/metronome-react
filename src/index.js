cd0d981725c5002fc55c29599ade04555e737066
Nick Coutsos <nick@coutsos.com>
2016-01-27 21:02:36 -0500

    Resizing without puredom

--- src/index.js
+++ src/index.js
@@ -3,42 +3,33 @@ import ReactDOM from 'react-dom';
 import components from './components';
 
 function set_metronome_size() {
-	var content = puredom('#content');
-	var metronome = content.query('.metronome');
+	let content = document.querySelector('#content'),
+		metronome = content.querySelector('.metronome'),
+		controls = metronome.querySelector('.controls'),
+		available = Math.min(content.offsetWidth - 40, content.offsetHeight - metronome.offsetTop),
+		outerLength = `${available}px`,
+		controlHeight = `${available / 7}px`;
 
-	var c_width = content.width({padding:false, margin:false, border:false});
-	var c_height = content.height();
-	var available_length = Math.min(c_width - 40, c_height - metronome.y());
-
-	metronome.css({
-		'height': available_length + 'px',
-		'width': available_length + 'px'
-	});
-
-	metronome.query('.controls').css({
-		'height': available_length / 7 + 'px',
-		'line-height': available_length / 7 + 'px',
-		'font-size': available_length / 7 + 'px'
-	});
+	metronome.style.height = metronome.style.width = outerLength;
+	controls.style.height = controls.style.lineHeight = controls.style.fontSize = controlHeight;
 }
-function set_content_size() {
-	var window_height = puredom('body').height();
-	var header_height = puredom('#header').height({border:true, padding:true, margin:true});
-	var footer_height = puredom('#footer').height({border:true, padding:true});
 
-	var new_height = window_height - header_height - footer_height;
+function setContentSize() {
+	let windowHeight = window.innerHeight,
+		headerHeight = document.querySelector('#header').offsetHeight,
+		footerHeight = document.querySelector('#footer').offsetHeight,
+		newHeight = windowHeight - headerHeight - footerHeight;
 
-	puredom('#content').css({'height': new_height + 'px'});
-
+	document.querySelector('#content').style.height = `${newHeight}px`;
 	set_metronome_size();
 }
 
-puredom(function(){
-	// var metronome = new Metronome('.metronome', 90);
-	ReactDOM.render(
-		React.createElement(components.Metronome, null),
-		document.getElementById('content')
-	);
-	set_content_size();
-});
-puredom(window).on('resize', set_content_size);
+
+
+ReactDOM.render(
+	React.createElement(components.Metronome, null),
+	document.getElementById('content')
+);
+
+window.addEventListener('resize', setContentSize);
+setContentSize();
